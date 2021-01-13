3c2bda04a6099e3a398735bcd788b9356479d18a
Shahboz <shahboz.turonov98@gmail.com>
2021-01-13 15:08:33 +0500

    Search Screen

--- lib/src/ui/bottom_view/search_screen.dart
+++ lib/src/ui/bottom_view/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_figma_one/src/app_theme.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -10,17 +11,147 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<String> listItems = [
+    "jordan 1",
+    "yeezy",
+    "jodan",
+    "off white",
+    "dior",
+    "travis scott",
+    "supreme",
+    "air force1",
+    "jordan 4",
+    "adidas",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.white,
-      appBar: AppBar(
-        elevation: 1,
-        title: Text("Bottom View"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // here the desired height
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0.0,
+          backgroundColor: AppTheme.white,
+          brightness: Brightness.light,
+        ),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Expanded(child: Container()),
+          Container(
+            margin: EdgeInsets.only(
+              top: 30,
+              left: 30,
+              right: 30,
+            ),
+            child: Text(
+              "Search",
+              style: TextStyle(
+                fontFamily: AppTheme.fontDisplay,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                height: 1.14,
+                color: AppTheme.black,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    left: 20,
+                    right: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppTheme.black5,
+                    borderRadius: BorderRadius.circular(
+                      100,
+                    ),
+                  ),
+                  height: 56,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width: 16),
+                      SvgPicture.asset("assets/images/search.svg"),
+                      SizedBox(width: 8),
+                      Expanded(
+                        child: Text(
+                          "Search",
+                          style: TextStyle(
+                            fontFamily: AppTheme.fontText,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            height: 1.5,
+                            color: AppTheme.black30,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      SvgPicture.asset("assets/images/filter.svg"),
+                      SizedBox(width: 16),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  right: 20,
+                ),
+                decoration: BoxDecoration(
+                  color: AppTheme.black5,
+                  borderRadius: BorderRadius.circular(
+                    100,
+                  ),
+                ),
+                height: 44,
+                width: 44,
+                child: Center(
+                  child: SvgPicture.asset(
+                    "assets/images/camera.svg",
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(
+                top: 0,
+                bottom: 120,
+              ),
+              itemCount: listItems.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return Container(
+                  height: 44,
+                  margin: EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                  ),
+                  child: Center(
+                    child: Text(
+                      listItems[index],
+                      style: TextStyle(
+                        fontFamily: AppTheme.fontText,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16,
+                        height: 1.5,
+                        color: AppTheme.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
